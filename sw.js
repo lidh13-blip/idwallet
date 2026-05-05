821daef317a0fa5bacded82382407c8cecbc44d7
lidh13-blip <lidh13@gmail.com>
2026-05-05 14:19:49 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-// 모든 캐시 삭제 및 서비스 워커 자동 해제
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-    .then(() => self.registration.unregister())
-  );
-});
