2e14c3328e0a36e333c121094b837ce7a52db288
lidh13-blip <lidh13@gmail.com>
2026-05-05 14:21:16 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+// 서비스 워커 비활성화 - 캐시 없음
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll())
+      .then(clients => clients.forEach(c => c.navigate(c.url)))
+  );
+});
